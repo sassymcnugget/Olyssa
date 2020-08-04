76b0a99e440ea61c7096c3a7c6505708db108a22
Olb2 <olgaborysovska@gmail.com>
2020-08-04 12:33:45 -0700

    created function to start and destroy user session, added logout link to home/show pages

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -5,10 +5,23 @@ const login  = (req, res) => {
     req.session.username = req.body.username 
     req.session.loggedIn = true 
     console.log(req.session)
-    res.redirect('/trips/')
-		
+    res.redirect('/trips')
 } 
 
+//destroy the seesion when user click on 'logout' link 
+const logout = (req, res) => {
+    req.session.destroy(err =>{
+        if (err){
+            console.log(err)
+            res.redirect('/trips')
+        }else{
+            res.redirect('/trips')
+        }
+    })
+}
+
+
 module.exports = {
-    login
+    login, 
+    logout
 }
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/sessions')
 
+//destroy the seesion when user click on 'logout' link 
+router.get('/logout', userController.logout)
+
+
 router.post('/', userController.login)
-
-
 
 module.exports = router
