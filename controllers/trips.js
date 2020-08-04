cc0086273c2ec057b67b5695d44a8d5244f2cc1f
Olb2 <olgaborysovska@gmail.com>
2020-08-03 20:23:20 -0700

    replaced create, deleteSight to sightseeing files, created deleteSight function to remove sights from show page

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,7 +1,6 @@
 const Trip = require("../models/trips")
 const Sightseeing = require("../models/sightseeings")
 
-
 //Path to '/trips/new/'
 const newOne = (req, res) => {
 	res.render("trips/new.ejs")
@@ -10,12 +9,7 @@ const newOne = (req, res) => {
 //create and save trip at '/trips/new/'
 const createTrip = async (req, res) => {
 	try{
-		// console.log(req.body) 
-		//create new var for whole object and see how you get it 
-		// object.location = req.body.latitude 
 		 const createdTrip = await Trip.create(req.body, (err, createdTrip) => {
-			// console.log(createdTrip)
-			// console.log(req.body)
 			res.redirect("/trips")
 		})
 	} catch (err) {
@@ -27,25 +21,24 @@ const index = async (req, res) => {
 	Trip.find({}, (err, allTrips) => {
 		res.render("home.ejs", {
 			trips: allTrips,
-		});
-	});
-};
+		})
+	})
+}
 
-//Path to individual trip show page('/trip/id') - retrieves data from Trips, Sightseeing models 
-//to show sights for each trip
-const show = async (req, res) => {
-	try{
-		const foundTrip =  await Trip.findById(req.params.id)
-		await foundTrip.populate('sightseeing') 
-        // await foundTrip.exec((err, foundTrip ) => {
-			res.render('trips/show.ejs', {
-            trip: foundTrip, 
-            sightseeing: foundTrip.sightseeing
-                })
-			
-	} catch (err){
-		console.log(err)
-		}
+//Path to the Show page 
+const show = (req, res) => {
+	Trip.findById(req.params.id)
+		.populate('sightseeing')
+        .exec((err, foundTrip ) => {
+            if(err) {
+                res.send(err)
+            } else {
+                res.render('trips/show.ejs', {
+                    trip: foundTrip, 
+					sightseeing: foundTrip.sightseeing
+				})
+			}
+		})
 	}
 
 // Delete Trip on show page 
@@ -60,27 +53,10 @@ const removeTrip = async(req, res) => {
 	}
 }
 
-///Create a new sightseeing on Trips show page ('/trip/id')
-const createTripsSight = async(req, res) => {
-	try{
-		const createdSight = await Sightseeing.create(req.body)  //first create the sight and then find Trip by id 
-		Trip.findById(req.params.id, (err, updatedTrip) =>{
-			updatedTrip.sightseeing.push(createdSight)
-			updatedTrip.save((err, result) => {
-				res.redirect(`/trips/${ req.params.id }`)
-			})
-		}) 	
-	}
-	catch (err){
-		console.log(err)  
-	}
-} 
-
 module.exports = {
 	index, 
 	show,
-	createTrip,  
 	newOne,
-	removeTrip, 
-	createTripsSight
+	createTrip,
+	removeTrip
 }
--- routes/sightseeings.js
+++ routes/sightseeings.js
@@ -1,45 +1,9 @@
 const express = require('express')
-const routerSightseeing = express.Router()
+const router = express.Router()
 const sightseeingController = require('../controllers/sightseeings')
 
-// // NEW ROUTE - send the user to the new fruit form page
-// router.get('/new', fruitsController.new)
+router.post('/', sightseeingController.create)
 
-// // CREATE ROUTE - will create a new fruit with data from the form
-// router.post('/', fruitsController.create)
+router.delete('/:id',sightseeingController.deleteSight )
 
-// // INDEX ROUTE - send all fruits in the response
-// router.get('/', fruitsController.index)
-
-// // SHOW ROUTE - send a single fruit in the response
-// router.get('/:id', fruitsController.show)
-
-// // DELETE ROUTE - remove a single fruit from the array and redirect the user to index
-// router.delete('/:id', (request, response) => {
-//     Fruit.findByIdAndDelete(request.params.id, (err, data) => {
-//         response.redirect('/fruits')
-//     })
-// })
-// // EDIT ROUTE - show the user a page with a form they can use to edit a specific item
-// router.get('/:id/edit', (request, response) => {
-//     Fruit.findById(request.params.id, (err, foundFruit) => {
-//         // rendering the edit.ejs
-//         response.render('edit.ejs', {
-//             // the fruit object
-//             fruit: foundFruit
-//         })
-//     })
-// }) 
-
-// // UPDATE ROUTE - provide the functionality allowing a user to change aspects of a given fruit
-// router.put('/:id', (request, response) => {
-//     // TERNARY OPERATOR - <condition> ? <TRUE result> : <FALSE result>
-//     request.body.readyToEat === 'on' ? request.body.readyToEat = true : request.body.readyToEat = false
-
-//     Fruit.findByIdAndUpdate(request.params.id, request.body, () => {
-//         response.redirect('/fruits')
-//     })
-// })
-
-//NOTE !!! router is exported with same name in trips and sightseeings 
-module.exports = routerSightseeing
+module.exports = router
