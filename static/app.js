984ff6575bb6fd7027204ea16d3f5993b0e376fd
sozettaslow <marissa.flores.1992@gmail.com>
2020-08-04 18:09:04 -0700

    refactored code in app.js

--- static/app.js
+++ static/app.js
@@ -30,42 +30,7 @@ function initMap() {
 	autocomplete.addListener("place_changed", function () {
 		infowindow.close();
 		let place = autocomplete.getPlace();
-		if (!place.geometry) {
-			// User entered the name of a Place that was not suggested and
-			// pressed the Enter key, or the Place Details request failed.
-			window.alert("No details available for input: '" + place.name + "'");
-			return;
-		}
-
-		// If the place has a geometry, then present it on a map.
-		if (place.geometry.viewport) {
-			map.fitBounds(place.geometry.viewport);
-		} else {
-			map.setCenter(place.geometry.location);
-			map.setZoom(17);
-		}
-		console.log("Spot: " + place.geometry.location);
-		let marker = addMarker(place.geometry.location);
-
-		let address = "";
-		if (place.address_components) {
-			address = [
-				(place.address_components[0] &&
-					place.address_components[0].short_name) ||
-					"",
-				(place.address_components[1] &&
-					place.address_components[1].short_name) ||
-					"",
-				(place.address_components[2] &&
-					place.address_components[2].short_name) ||
-					"",
-			].join(" ");
-		}
-
-		infowindowContent.children["place-icon"].src = place.icon;
-		infowindowContent.children["place-name"].textContent = place.name;
-		infowindowContent.children["place-address"].textContent = address;
-		infowindow.open(map, marker);
+		focusPlace(place);
 	});
 
 	// Sets a listener on a radio button to change the filter type on Places
@@ -88,4 +53,39 @@ function initMap() {
 			console.log("Checkbox clicked! New state=" + this.checked);
 			autocomplete.setOptions({ strictBounds: this.checked });
 		});
+}
+
+function focusPlace(place) {
+	if (!place.geometry) {
+		// User entered the name of a Place that was not suggested and
+		// pressed the Enter key, or the Place Details request failed.
+		window.alert("No details available for input: '" + place.name + "'");
+		return;
+	}
+
+	// If the place has a geometry, then present it on a map.
+	if (place.geometry.viewport) {
+		map.fitBounds(place.geometry.viewport);
+	} else {
+		map.setCenter(place.geometry.location);
+		map.setZoom(17);
+	}
+	let marker = addMarker(place.geometry.location);
+
+	let address = "";
+	if (place.address_components) {
+		address = [
+			(place.address_components[0] && place.address_components[0].short_name) ||
+				"",
+			(place.address_components[1] && place.address_components[1].short_name) ||
+				"",
+			(place.address_components[2] && place.address_components[2].short_name) ||
+				"",
+		].join(" ");
+	}
+
+	infowindowContent.children["place-icon"].src = place.icon;
+	infowindowContent.children["place-name"].textContent = place.name;
+	infowindowContent.children["place-address"].textContent = address;
+	infowindow.open(map, marker);
 }
