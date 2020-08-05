7a7e4c37754c55cb15147503161f6603e69eaf9d
sozettaslow <marissa.flores.1992@gmail.com>
2020-08-04 17:13:53 -0700

    updating info window information

--- static/app.js
+++ static/app.js
@@ -51,12 +51,12 @@ function initMap() {
 			map.fitBounds(place.geometry.viewport);
 		} else {
 			map.setCenter(place.geometry.location);
-			map.setZoom(17); // Why 17? Because it looks good.
+			map.setZoom(17);
 		}
 		//marker.setPosition(place.geometry.location);
 		console.log("Spot: " + place.geometry.location);
 		//marker.setVisible(true);
-		addMarker(place.geometry.location);
+		let marker = addMarker(place.geometry.location);
 
 		let address = "";
 		if (place.address_components) {
@@ -76,6 +76,7 @@ function initMap() {
 		infowindowContent.children["place-icon"].src = place.icon;
 		infowindowContent.children["place-name"].textContent = place.name;
 		infowindowContent.children["place-address"].textContent = address;
+		//infowindowContent.children["place-content"].textContent = contentString;
 		infowindow.open(map, marker);
 	});
 
@@ -99,10 +100,10 @@ function initMap() {
 			console.log("Checkbox clicked! New state=" + this.checked);
 			autocomplete.setOptions({ strictBounds: this.checked });
 		});
-
-	map.addListener("click", (event) => {
-		addMarker(event.latLng);
-	});
+	//add marker tester
+	// map.addListener("click", (event) => {
+	// 	addMarker(event.latLng);
+	// });
 }
 
 // Adds a marker to the map and push to the array.
@@ -116,6 +117,18 @@ function addMarker(location) {
 
 	return marker;
 }
+
+const contentString =
+	"<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+	"sandstone rock formation in the southern part of the " +
+	"Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+	"south west of the nearest large town, Alice Springs; 450&#160;km " +
+	"(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+	"features of the Uluru - Kata Tjuta National Park. Uluru is " +
+	"sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+	"Aboriginal people of the area. It has many springs, waterholes, " +
+	"rock caves and ancient paintings. Uluru is listed as a World " +
+	"Heritage Site.";
 
 // Sets the map on all markers in the array.
 function setMapOnAll(map) {
@@ -139,3 +152,6 @@ function deleteMarkers() {
 	clearMarkers();
 	markers = [];
 }
+
+//creating array of destinations that are held from where you click on the map
+let sightseeingArray = [];
