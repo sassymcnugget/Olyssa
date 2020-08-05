a2ecf7fafdd8f24b51d135cd48e8326539470d11
sozettaslow <marissa.flores.1992@gmail.com>
2020-08-04 17:46:32 -0700

    created mapUtils to store map information so app.js is easier to read through

---
+++ static/mapUtils.js
@@ -0,0 +1,34 @@
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+	const marker = new google.maps.Marker({
+		position: location,
+		map: map,
+	});
+
+	markers.push(marker);
+
+	return marker;
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+	for (let i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+	}
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+	setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+	setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+	clearMarkers();
+	markers = [];
+}
